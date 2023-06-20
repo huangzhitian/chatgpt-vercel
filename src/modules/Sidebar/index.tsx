80bb33ab59a6bc66eaac58ed91132d213c571a8b
Feng <129321746+huangzhitian@users.noreply.github.com>
2023-06-20 15:51:43 +0800

    Update index.tsx

--- src/modules/Sidebar/index.tsx
+++ src/modules/Sidebar/index.tsx
@@ -80,14 +80,14 @@ const Sidebar: FC<{
 
   return (
     <div className="flex flex-col h-full bg-white">
-      <div className="p-4 mt-2 flex items-baseline justify-between">
+      <div style="display:none;" className="p-4 mt-2 flex items-baseline justify-between">
         <span className="text-3xl text-gradient font-[800]">ChatGPT</span>
         <a
           href="https://github.com/GPTGenius/chatgpt-vercel"
           target="_blank"
           rel="noreferrer"
         >
-          <i className="ml-2 ri-github-fill text-xl" />
+          <i  className="ml-2 ri-github-fill text-xl" />
         </a>
       </div>
       <div className="p-2 flex items-center justify-between mb-4">
